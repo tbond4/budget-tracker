99d6d360d246027bd34cabb528249a727d1d2e64
Tristan Bond <tristanebond@gmail.com>
2021-02-19 19:29:31 -0800

    indexeddb.js file update

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -2,7 +2,8 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-    const pendingStore = db.createObjectStore("pending", {autoIncrement: "true"});
+    const db = event.target.result;
+    db.createObjectStore("pending", {autoIncrement: "true"});
   };
 
   request.onsuccess = function (event) {
@@ -12,21 +13,26 @@ request.onupgradeneeded = function (event) {
       checkDatabase();
     }
   };
+
+  request.onerror = function(event) {
+    console.log(event.target.errorCode);
+  };
+
   function saveRecord(record) {
     const transaction = db.transaction(["pending"], "readwrite");
-    // create a transaction on the pending db with readwrite access
+    
     const pendingStore = transaction.objectStore("pending");
-    // access your pending object store
+   
     pendingStore.add(record);
-    // add record to your store with add method.
+   
   }
   
   function checkDatabase() {
-    // open a transaction on your pending db
+    
     const transaction = db.transaction(["pending"], "readwrite");
-    // access your pending object store
+    
     const pendingStore = transaction.objectStore("pending");
-    // get all records from store and set to a variable
+    
     const getAll = pendingStore.getAll();
   
     getAll.onsuccess = function () {
@@ -41,11 +47,11 @@ request.onupgradeneeded = function (event) {
         })
           .then((response) => response.json())
           .then(() => {
-            // if successful, open a transaction on your pending db
+            
             const transaction = db.transaction(["pending"], "readwrite");
-            // access your pending object store
+           
             const pendingStore = transaction.objectStore("pending");
-            // clear all items in your store
+            
             pendingStore.clear();
           });
       }
